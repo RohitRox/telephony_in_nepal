67d5e6c93b6125372964d671cb9df6fdb2c37e41
sweetanzell <sweetanzell@gmail.com>
2013-07-07 15:24:46 +0545

    Theming of policies and js for operators

---
+++ js/script.js
@@ -0,0 +1,102 @@
+
+$(document).ready(function(){
+
+$('.circles').click(function(){
+
+	var that = $(this);
+	var id = $(this).attr('id');
+
+	if($('#circle').hasClass(id)) 
+	{
+		if($('#circle').hasClass('small')) {
+
+			$('#circle').animate({
+                        'width'     :'400px',
+                        'height'    :'400px',
+                        'top'       :'40px',
+                        'left'      :'40px',
+                        'opacity'   :'1.0',
+                        'border-radius' : '50%',
+                        'padding' : '40px'
+                       },1200,'easeOutCirc', function(){
+                       	$('#circle div').fadeIn('slow');
+                       });
+
+			$('#circle').removeClass('small').css('text-indent','0px');
+		}
+		else 
+		{
+			$('#circle').animate({
+                        'width'     :'32px',
+                        'height'    :'32px',
+                        'top'       :'50%',
+                        'left'      :'50%',
+                        'opacity'   :'1',
+                        'padding'   :'0px'
+                        },300,'easeInCirc');
+			$('#circle div').fadeOut();
+			$('#circle').addClass('small').css('text-indent','-9999px');
+		}
+		
+	}
+	else 
+	{	
+
+		$('#circle div').hide();
+		var abc = $(that).find('.text').html();
+		$('#circle div').html(abc);
+
+		if($('#circle').hasClass('small')) {
+
+					$('#circle').animate({
+                        'width'     :'400px',
+                        'height'    :'400px',
+                        'top'       :'40px',
+                        'left'      :'40px',
+                        'opacity'   :'1.0',
+                        'border-radius' : '50%',
+                        'padding' : '40px'
+	                    },300,'easeInCirc', function(){ $('#circle div').fadeIn('slow'); });
+		}
+		else{
+			$('#circle').animate({
+						'width'     :'32px',
+                        'height'    :'32px',
+                        'top'       :'50%',
+                        'left'      :'50%',
+                        'opacity'   :'1',
+                        'padding'   :'0px'
+			}).animate({
+	                        'width'     :'400px',
+	                        'height'    :'400px',
+	                        'top'       :'40px',
+	                        'left'      :'40px',
+	                        'opacity'   :'1.0',
+	                        'border-radius' : '260px',
+	                        'padding' : '40px'
+	                       },300,'easeInCirc', function(){ $('#circle div').fadeIn('slow'); });
+		}
+		$('#circle').attr('class', id);
+		
+	}
+	
+
+});
+
+
+$('.circles').click(function(){
+ $('#circle span').click(function(){
+    $('#circle').animate({
+                        'width'     :'32px',
+                        'height'    :'32px',
+                        'top'       :'50%',
+                        'left'      :'50%',
+                        'opacity'   :'1',
+                        'padding'   :'0px' 
+                        },300,'easeOutCirc');
+            $('#circle div').fadeOut('fast');
+            $('#circle').addClass('small').css('text-indent','-9999px');
+ });
+});
+
+});
