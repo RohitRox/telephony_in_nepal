d0fe4d9e69293e26fedd52603e771b3d7a21144d
sweetanzell <sweetanzell@gmail.com>
2013-07-08 07:33:31 +0545

    Theming and js for graphs

--- js/script.js
+++ js/script.js
@@ -99,4 +99,166 @@ $('.circles').click(function(){
  });
 });
 
+
 });
+
+$(document).ready(function(){
+
+  setTimeout(function(){
+    $(".year1").fadeIn('slow');
+    $(".percent1").fadeIn('slow');
+  },2000);
+
+
+});
+
+
+  var data = [
+  {date: "14 June, 2010", tele_penetration: 30.24, internet_penetration: 4.56},
+  {date: "15 November, 2010", tele_penetration: 36.37, internet_penetration: 10.02},
+  {date: "15 June, 2011", tele_penetration: 44.9, internet_penetration: 14.4},
+  {date: "14 December, 2011", tele_penetration: 56.46, internet_penetration: 15.5},
+  {date: "14 June, 2012", tele_penetration: 63.76, internet_penetration: 17.34},
+  {date: "15 December, 2012", tele_penetration: 70.61, internet_penetration: 22.22},
+  {date: "14 May , 2013", tele_penetration: 77.11, internet_penetration: 25.67}
+  ];
+  var width = 920, height = 400;
+  var gap = Math.round((width/data.length));
+
+  function stage_init(){
+
+    window.stage = new Kinetic.Stage({
+      container: 'canvas_telecom_glance',
+      width: width,
+      height: height,
+      fill: "def"
+    });
+
+    window.layer = new Kinetic.Layer();
+
+    var line = new Kinetic.Line({
+      points: [0, height, width, height],
+      stroke: "#fff",
+      strokeWidth: 2,
+    });
+    layer.add(line);
+
+    var quater = Math.round((height/4));
+    var tmp = 0;
+    for(var i=1; i<=4;i++){
+      tmp += quater;
+      var line = new Kinetic.Line({
+        points: [0,height-tmp,width,height-tmp],
+        stroke: "#fff",
+        strokeWidth: 1
+      });
+      var per = new Kinetic.Text({x:4,y:height-tmp+4, text: 25*i+" %", fill: "#fff", fontFamily: "quicksandbook"});
+      layer.add(per);
+      layer.add(line);
+    }
+    stage.add(layer);
+  }
+
+
+	function telecom_at_glance_init(data_selector, color_pref){
+
+    var layer = new Kinetic.Layer();
+    var len = data.length, k =0,x = 0;
+    window.p_arr = [];
+
+    window.handle = setInterval(function(){
+      if( k == len-1)
+        {
+          clearInterval(handle);
+          join_points();
+          return false;
+        }
+      x = x + gap;
+      var self = data[k];
+      var y = Math.round(((self[data_selector]/100)*height));
+
+      console.log(self[data_selector]);
+      p_arr.push(x,height - y);
+
+      var circ = new Kinetic.Circle({
+        x: x, y: height - y,
+        fill: '#fff',
+        // strokeWidth: 4,
+        // radius: 0,
+        // stroke: "white",
+      });
+
+      layer.add(circ);
+
+      var poly = new Kinetic.Polygon({
+        points: [x-40, height-y-25, x-10, height-y-25, x, height-y-15, x+10, height-y-25, x+40, height-y-25, x+40, height-y-90, x-40, height-y-90],
+        fill: '#fff'
+      });
+
+      layer.add(poly);
+
+      var perce = new Kinetic.Text({
+        x:x-35,
+        y:height-y-69,
+        text: self[data_selector]+"%" ,
+        fontFamily: 'quicksandbook',
+        fill: '#00b1e1',
+        stroke: '#00b1e1',
+        strokeWidth: 1,
+        fontSize: '20',
+        fontWeight: 'bold',
+        'z-index': '11'
+      });
+      layer.add(perce);
+
+      (new Kinetic.Tween({
+        node: circ,
+        radius: 8,
+        duration: .5,
+        easing : Kinetic.Easings.BounceEaseIn
+      })).play();
+
+      var date = new Kinetic.Text({x:x-40,y:height-15, text: self.date , fill: "#fff",fontFamily: 'quicksandbook', fontSize: '12'});
+      layer.add(date);
+      stage.add(layer);
+      k++;
+    },800);
+
+    function join_points(){
+      var line_join = new Kinetic.Line({
+          points: p_arr,
+          stroke: '#fff',
+          strokeWidth: 4
+        });
+      layer.add(line_join);
+      stage.add(layer);
+    }
+
+    return layer;
+
+  }
+  var tele_layer = null, internet_layer = null;
+  setTimeout(function(){
+  	stage_init();
+    tele_layer = telecom_at_glance_init("tele_penetration","FB5352");
+  },2000);
+
+  $('.legend').click(function(){
+    var _this = $(this);
+    _this.toggleClass('active');
+    if( _this.data("val") == "tele_penetration"){
+      if (internet_layer != null)
+        internet_layer.hide();
+      if (tele_layer === null)
+        tele_layer = telecom_at_glance_init(_this.data("val"), _this.data("color"));
+      else
+        tele_layer.show();
+    }else{
+      tele_layer.hide();
+      if (internet_layer === null)
+        internet_layer = telecom_at_glance_init(_this.data("val"), _this.data("color"));
+      else
+        internet_layer.show();
+    }
+  });
+
